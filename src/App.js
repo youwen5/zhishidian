9803bb8c379125236aadc862f5309684e6d3ef8a
couscousdude <youwenw@gmail.com>
2020-06-28 23:10:32 -0700

    prepared for routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import MenuNavBar from './components/Navbar.js';
 import Bottomnavigation from './components/BottomNavigation.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {

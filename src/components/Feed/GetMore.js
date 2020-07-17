7c15befbfbd445b1daa907e485fdbe5644099dba
couscousdude <youwenw@gmail.com>
2020-07-16 17:11:23 -0700

    Added Load More functionality

---
+++ src/components/Feed/GetMore.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { IconButton } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    button: {
+        left: "50%",
+        transform: "translateX(-50%)"
+    }
+});
+
+export default function GetMore(props) {
+    const classes = useStyles();
+
+    const handleClick = () => {
+        props.getMorePosts(props.startId);
+        console.log(props.startId);
+    }
+
+    return(
+    <IconButton aria-label="get more" className={classes.button} onClick={handleClick}>
+        <ExpandMoreIcon />
+    </IconButton>
+    );
+}
--- src/components/databaseManagement/getAll.js
+++ src/components/databaseManagement/getAll.js
@@ -4,9 +4,9 @@ const config = require('./config.json');
 export default async function getAll(startId=0) {
     try {
         const response = await axios.get(`${config.api.invokeUrl}/posts/${startId}`);
-        // return sortArrayByISO(response.data);
         return response.data;
     } catch(error) {
         console.log(`Unexpected error occurred posting: ${error}`);
+        throw Error;
     }
 }
